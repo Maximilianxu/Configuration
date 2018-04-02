500db1b8164f86636debc89480f19bd0728e811a
Maximilianxu <maximilian.xu.2015@gmail.com>
2018-04-02 14:27:14 +0800

    mineral changes

--- model/variable.py
+++ model/variable.py
@@ -6,6 +6,11 @@ class Variable:
         self.val = val
         self.dom = dom
     
+    # 测试某个值是否有效
     def is_valid(self, val_ind):
         return self.dom.is_valid(val_ind)
+    
+    # 反转某个值的有效性
+    def reverse(self, val_ind):
+        self.dom.reverse(val_ind)
     
