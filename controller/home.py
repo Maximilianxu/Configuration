7fdf485f57dd54cab85c4483fb5ec7eb230e0597
Maximilianxu <maximilian.xu.2015@gmail.com>
2018-05-06 11:17:49 +0800

    登录（换成邮箱验证）以及一些细节改动

---
+++ controller/home.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from flask import Blueprint,flash, redirect, url_for, render_template, request, make_response, session
+from Configuration.inventory.user_inv import insert_user, find_user_by_email
+from Configuration.model.user import User
+import smtplib
+from email.mime.text import MIMEText
+
+home = Blueprint('home', __name__, template_folder='templates')
+
+
+@home.route('/login', methods=['POST'])
+def login():
+    log_email = request.form.get('log_email')
+    password = request.form.get('log_pswd')
+    user = find_user_by_email(log_email)
+    print('========>', log_email)
+    print('========>', password)
+    print('========>', user.password)
+    if user.password == password:
+        resp = make_response(render_template('home.html', login=True))
+        resp.set_cookie('user_email', log_email)
+        return resp
+    else:
+        return render_template('home.html', login=False)
+
+
+# Send the message via our own SMTP server.
+server = smtplib.SMTP_SSL()
+
+@home.route('/register', methods=['POST'])
+def register():
+    reg_email = request.form.get('log_email')
+    name = request.form.get('log_name')
+    password = request.form.get('log_pswd')
+    user = User(reg_email, name, password)
+    server.connect('smtp.qq.com', 465)
+    server.login("193559882", "idhqibmcwajmbjgb")
+    mail_text = '''<body>欢迎您注册Config，请单击如下链接验证邮箱，以完成注册，谢谢！
+    <br><a href='http://localhost:5000/validate?name=%s'>注册验证</a></body>'''\
+    % name
+    msg = MIMEText(mail_text, 'html')
+    msg['Subject'] = 'CONFIG 邮箱验证'
+    from_addr = '193559882@qq.com'
+    msg['From'] = from_addr
+    msg['To'] = reg_email
+    server.sendmail(from_addr, reg_email, msg.as_string())
+    server.quit()
+    session[name] = user.toJSON()
+    return '一封验证邮件已经发送到您的邮箱，请查看并进行验证，谢谢！'
+
+@home.route('/validate')
+def validate():
+    user_name =  request.args.get('name', '')
+    import json
+    user = lambda: None
+    user.__dict__ = json.loads(session[user_name])
+    try:
+        insert_user(user)
+        session.pop(user_name, None)
+        flash('register success!')
+        return redirect(url_for('index'))
+    except Exception as e:
+        session.pop(user_name, None)
+        return 'register failure'
--- schema.sql
+++ schema.sql
@@ -1,3 +1,11 @@
+DROP TABLE IF EXISTS c_user;
+CREATE TABLE c_user(
+    email VARCHAR(64),
+    name VARCHAR(16) UNIQUE,
+    password VARCHAR(32),
+    PRIMARY KEY(email)
+);
+
 DROP TABLE IF EXISTS c_order;
 CREATE TABLE c_order(
     id INT AUTO_INCREMENT,
@@ -7,5 +15,6 @@ CREATE TABLE c_order(
     order_date DATE,
     price FLOAT,
     order_state VARCHAR(4),
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY (user_email) REFERENCES c_user(email)
 );
