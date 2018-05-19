4b5fcf5df9fb05b7d0e9aa6ba5def7eb6b2fecad
chen jn <chenjn_amtf@qq.com>
2018-05-19 11:02:21 +0800

    config产品树

--- controller/order_generator.py
+++ controller/order_generator.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, jsonify
 import datetime
+from Configuration.inventory.component_inv import find_subcomponents_id_name
 from Configuration.inventory.order_inv import find_all_orders_by_email, insert_order
 from Configuration.model.order import Order
 order_generator = Blueprint('order_generator', __name__, template_folder='templates')
 
 @order_generator.route('/config')
 def config():
-    return render_template('config.html')
+    root_component_id = session['root_component_id']
+    root_component_name = session['root_component_name']
+    subcomponents = find_subcomponents_id_name(root_component_id)
+    return render_template('config.html',\
+            root_component_id=root_component_id, root_component_name=root_component_name,\
+            subcomponents=subcomponents)
+
+# @order_generator.route('/config/subcomponents', methods=['POST'])
+# def find_subcomponents():
+#     data = request.get_json()
+#     father_component_id = data['id']
+#     subcomponents = find_subcomponents_id_name(father_component_id)
+#     return jsonify(subcomponents)
 
 @order_generator.route('/order')
 def order():
